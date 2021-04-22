bf70b1b1958c421b9708a7a95c771a6ffd52d4b9
Ricky Januari <rickyjanuari@gmail.com>
2021-04-22 07:31:20 +0700

    fix fragment error

--- app/src/main/java/com/amarDigital/nastaqeem/ui/MainActivity.kt
+++ app/src/main/java/com/amarDigital/nastaqeem/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.amarDigital.nastaqeem.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.amarDigital.nastaqeem.R
 import com.amarDigital.nastaqeem.databinding.ActivityMainBinding
+import com.amarDigital.nastaqeem.fragments.HomeFragment
+import com.amarDigital.nastaqeem.fragments.JurnalFragment
+import com.amarDigital.nastaqeem.fragments.MoreFragment
+import com.amarDigital.nastaqeem.fragments.QuranFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +20,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navigation
-
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_evaluasi))
-        // setupActionBarWithNavController(navController, appBarConfiguration)
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        NavigationUI.setupWithNavController(navView, navController)
-        // navView.setupWithNavController(navController)
+        val navigation: BottomNavigationView = binding.navigation
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val fragment = HomeFragment.newInstance()
+        addFragment(fragment)
+    }
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val fragment = HomeFragment.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_jurnal -> {
+                val fragment = JurnalFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_quran -> {
+                val fragment = QuranFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_more -> {
+                val fragment = MoreFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .replace(R.id.content, fragment, fragment.javaClass.getSimpleName())
+            .commit()
     }
 }
